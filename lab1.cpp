3a25c26a91d5c8fd2adffe56b33d508b5459d830
SandalovKY <sandaloff.constantin@yandex.ru>
2022-09-23 18:39:50 +0300

    Modified

--- lab1.cpp
+++ lab1.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <iostream>
 #include <bitset>
+#include <set>
+#include <algorithm>
 
 class Impl
 {
@@ -12,6 +14,7 @@ private:
     int m_ind{};
     int m_p{};
     bool m_pw{};
+    bool m_inf{};
 
     static int countSetBits(int num)
     {
@@ -25,9 +28,10 @@ private:
     }
 
 public:
-    Impl(int num) : m_num(num), m_ind(countSetBits(num)), m_p(0), m_pw(false) {}
-    Impl(const Impl& other) : m_num(other.m_num), m_ind(other.m_ind), m_p(other.m_p), m_pw(false) {}
-    bool operator==(Impl& other)
+    Impl(int num, bool inf) : m_num(num), m_ind(countSetBits(num)), m_p(0), m_pw(false), m_inf(inf) {}
+    // Due to semantic of the creation the new implicant, its m_pw value should be false
+    Impl(const Impl& other) : m_num(other.m_num), m_ind(other.m_ind), m_p(other.m_p), m_pw(false), m_inf(other.m_inf) {}
+    bool operator==(Impl& other) const
     {
         return m_num == other.m_num && m_p == other.m_p;
     }
@@ -55,7 +59,8 @@ public:
     int getNum() const { return m_num; }
     int getInd() const { return m_ind; }
     int getPatch() const { return m_p; }
-    bool isPatched() const { return m_pw; } 
+    bool isPatched() const { return m_pw; }
+    bool isInf() const { return m_inf; }
 };
 
 
@@ -63,20 +68,106 @@ class DNF
 {
 private:
     std::vector<Impl> m_data;
+
+    std::set<int> removeRedundancy1(const std::vector<std::set<int> >& coverage)
+    {
+        std::set<int> ansver{};
+        std::vector<std::pair<int, std::pair<int, int>>> segments{};
+        for (int ind = 0; ind < coverage.size(); ++ind)
+        {
+            auto& covSet = coverage[ind];
+            if (covSet.size() > 0)
+            {
+                int start = *covSet.begin();
+                int prev = *covSet.begin();
+                auto curr = ++covSet.begin();
+                while(curr != covSet.end())
+                {
+                    int curr_val = *curr;
+                    if (curr_val != prev + 1)
+                    {
+                        segments.push_back({ ind, { start, prev } });
+                        start = curr_val;
+                    }
+                    prev = curr_val;
+                    ++curr;
+                }
+                segments.push_back({ ind, { start, prev } });
+            }
+        }
+        std::sort(segments.begin(), segments.end(), [](auto& a, auto& b){
+            return a.second.first < b.second.first;
+        });
+
+        int max = -1;
+        int currX = 0;
+        int maxSeg = -1;
+
+        for (const auto& segment: segments)
+        {
+            if (segment.second.first > currX)
+            {
+                ansver.insert(maxSeg);
+                currX = max; 
+            }
+            if (segment.second.second > max)
+            {
+                max = segment.second.second;
+                maxSeg = segment.first;
+            }
+        }
+        if ((--segments.end())->second.first >= currX)
+        {
+            ansver.insert(maxSeg);
+            currX = max;
+        }
+        return ansver;
+    }
+
+    std::set<int> removeRedundancy2(std::vector<std::set<int> > coverage,
+        std::vector<std::set<int> > coverageOnBars)
+    {
+        std::set<int> ansver{};
+        std::sort(coverageOnBars.begin(), coverageOnBars.end(), [](const auto& a, const auto& b){
+            return a.size() < b.size();
+        });
+
+        for (int ind = 0; ind < coverageOnBars.size(); ++ind)
+        {
+            auto result = std::max_element(coverageOnBars[ind].begin(), coverageOnBars[ind].end(), [&coverage](int a, int b){
+                return coverage[a].size() > coverage[b].size();
+            });
+
+        }
+
+        auto& smallestBar = coverageOnBars[0];
+
+        
+
+        return ansver;
+    }
 public:
     DNF(std::string sourceDNF)
     {
         for (int pos = 0; pos < sourceDNF.size(); ++pos)
         {
-            if (sourceDNF[pos] == '1' or sourceDNF[pos] == '-')
+            bool isInf = sourceDNF[pos] == '-';
+            if (sourceDNF[pos] == '1' or isInf)
             {
-                m_data.emplace_back(Impl(pos));
+                m_data.emplace_back(Impl(pos, isInf));
             }
         }
     }
     void minimize()
     {
-        std::vector<Impl> src = m_data;
+        std::vector<Impl> src{};
+        for (const auto& el: m_data)
+        {
+            if (!el.isInf())
+            {
+                src.push_back(el);
+            }
+        }
         // First stage: getting TDNF (Terminated Disjunctive Normal FOrm)
         bool canMinimize{ true };
         while (canMinimize)
@@ -109,21 +200,49 @@ public:
 
         // Second stage: minimization of TDNF, getting MDNF (minimum)
 
-        std::vector<std::pair<int, int>>
+        std::vector<std::set<int> > coverage{ m_data.size() };
+        std::vector<std::set<int> > coverageOnBars{ src.size() };
 
-        for (const auto& nt : m_data)
+        for (int ind = 0; ind < m_data.size(); ++ind)
         {
-            for (const auto& nt : src)
+            std::set<int> implCov{};
+            for (int jnd = 0; jnd < src.size(); ++jnd)
             {
-
+                if ((src[jnd].getNum() & ~m_data[ind].getPatch()) == m_data[ind].getNum())
+                {
+                    implCov.insert(jnd);
+                    coverageOnBars[jnd].insert(ind);
+                }
             }
+            coverage[ind] = std::move(implCov);
         }
+
+        std::set<int> ansver = removeRedundancy1(coverage);
+        std::vector<Impl> new_data{};
+        for (auto& el: ansver)
+        {
+            new_data.push_back(m_data[el]);
+        }
+        m_data = new_data;
     }
     void print(std::ostream& outStream) const
     {
         for (const auto& impl: m_data)
         {
-            outStream << std::bitset<4>(impl.getNum()) << '\t' << std::bitset<4>(impl.getPatch()) << '\t' << impl.getInd() << std::endl;
+            std::bitset<4> patch(impl.getPatch());
+            std::bitset<4> num(impl.getNum());
+            for (int ind = patch.size() - 1; ind >= 0; --ind)
+            {
+                if (patch[ind] == 1)
+                {
+                    outStream << '-';
+                }
+                else
+                {
+                    outStream << num[ind];
+                }
+            }
+            outStream << std::endl;
         }
     }
     Impl& getImpl(int);
@@ -159,5 +278,8 @@ int main(int argc, char* argv[])
     sourceDNF.minimize();
     std::cout << "\n-------------------------------------\n";
     sourceDNF.print(std::cout);
+    std::ofstream out{ "mdnf.txt" };
+    sourceDNF.print(out);
+    out.close();
     return 0;
 }
